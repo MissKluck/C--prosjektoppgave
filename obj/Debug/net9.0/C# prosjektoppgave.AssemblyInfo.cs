dcdba31e9b7e3c9ce19b5d147588eeda5604184e
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-22 11:01:19 +0100

    initial setup of website + added favicon

--- obj/Debug/net9.0/C# prosjektoppgave.AssemblyInfo.cs
+++ obj/Debug/net9.0/C# prosjektoppgave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# prosjektoppgave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a151810b6fb2040007be77edb61428a290033d0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# prosjektoppgave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# prosjektoppgave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
