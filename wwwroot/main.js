3bf9ada65dc645d7199df35865e8f46b22a3a74e
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-25 13:56:40 +0100

    setup for messaging server and checking if it works

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,2 +1,29 @@
-// connect to the api
+// Connect to the api
 import { url } from './server.js';
+
+// Find elements in HTML document
+const ul = document.querySelector('#char-container');
+
+// Message server
+const response = await fetch(url);
+console.log(response);
+
+// Check that everything works
+async function checkapi(api) {
+  try {
+    BoxCreators();
+  } catch {
+    throw new console.error(api.statusText);
+  }
+}
+
+// Convert from JSON to JavaScript Object
+const dta = await response.json();
+console.log(data);
+
+checkapi(response);
+
+// Create the boxes
+//function BoxCreators() {
+    for (let i = 0; i < )
+//}
