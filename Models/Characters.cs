9f108d4183e32e6ed925a0fcedf976fe002942ca
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-26 19:33:35 +0100

    removed blob variable

--- Models/Characters.cs
+++ Models/Characters.cs
@@ -6,6 +6,5 @@ public class Characters
     public required string? Name { get; set; }
     public required int Age { get; set; }
     public required string? Film { get; set; }
-    public Blob image { get; set; }
     public required string About { get; set; }
 }
