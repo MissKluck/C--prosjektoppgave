9ef9be62cb7c36e97d7a157a9843487b822cdc92
MissKluck <sigrid.b.kluck@gmail.com>
2024-12-05 10:05:42 +0100

    added imageurl to model

--- Models/Characters.cs
+++ Models/Characters.cs
@@ -7,4 +7,5 @@ public class Characters
     public required int Age { get; set; }
     public required string? Film { get; set; }
     public required string About { get; set; }
+    public string? ImageUrl { get; set; }
 }
